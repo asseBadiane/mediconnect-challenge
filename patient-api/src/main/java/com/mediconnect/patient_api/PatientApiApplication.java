4e0bad16edac3f8b80ea755f344878ce658512fe
asseBadiane <badianeasse99@gmail.com>
2025-06-27 22:56:50 +0000

    ✨ Atransactional management

--- patient-api/src/main/java/com/mediconnect/patient_api/PatientApiApplication.java
+++ patient-api/src/main/java/com/mediconnect/patient_api/PatientApiApplication.java
@@ -2,12 +2,12 @@ package com.mediconnect.patient_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class PatientApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PatientApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PatientApiApplication.class, args);
+    }
 }
