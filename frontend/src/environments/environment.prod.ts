a507ed7a6f5d86b2a9088935cd38da3998c59bcb
asseBadiane <badianeasse99@gmail.com>
2025-06-29 11:59:30 +0000

    🔧 Add file env

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://your-production-url.com',
+  patientApiUrl: 'https://your-production-url.com/patient-api/fhir',
+  conditionApiUrl: 'https://your-production-url.com/condition-api/fhir',
+  apiTimeout: 15000
+};
