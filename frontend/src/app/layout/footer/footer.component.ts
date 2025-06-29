f7839f3fa878165ddc31926b15833250b3e8e5fb
asseBadiane <badianeasse99@gmail.com>
2025-06-29 15:49:38 +0000

    📱 Add footer

--- frontend/src/app/layout/footer/footer.component.ts
+++ frontend/src/app/layout/footer/footer.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  currentYear = new Date().getFullYear();
 }
