1dbcca37c4c68005058ef1d3c4f537fea9a8b248
asseBadiane <badianeasse99@gmail.com>
2025-06-29 12:12:24 +0000

    📦  Add the differents components for Patient

---
+++ frontend/src/app/features/patients/pages/patient-profile-page/patient-profile-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-profile-page',
+  imports: [],
+  templateUrl: './patient-profile-page.component.html',
+  styleUrl: './patient-profile-page.component.scss'
+})
+export class PatientProfilePageComponent {
+
+}
---
+++ frontend/src/app/features/patients/pages/patients-page/patients-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patients-page',
+  imports: [],
+  templateUrl: './patients-page.component.html',
+  styleUrl: './patients-page.component.scss'
+})
+export class PatientsPageComponent {
+
+}
