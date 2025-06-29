12e98a9b0e2e5d00d797c7e4a962e2f7b40bf319
asseBadiane <badianeasse99@gmail.com>
2025-06-29 21:44:07 +0000

    🏗️ move the profile page in another folder

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './features/dashboard/dashboard/dashboard.component';
 import { PatientListComponent } from './features/patients/components/patient-list/patient-list.component';
 import { PatientFormComponent } from './features/patients/components/patient-form/patient-form.component';
-import { PatientProfilePageComponent } from './features/patients/pages/patient-profile-page/patient-profile-page.component';
+import { PatientProfilePageComponent } from './features/patients/components/patient-profile-page/patient-profile-page.component';
 import { UpdatePatientComponent } from './features/patients/components/update-patient/update-patient.component';
 
 
