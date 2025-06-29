e3ba92b489231d9b25669342c71619ec0b5d94db
asseBadiane <badianeasse99@gmail.com>
2025-06-29 20:28:52 +0000

    🚧 config CORS on Condition APi

--- condition-api/src/main/java/com/mediconnect/condition_api/config/WebConfig.java
+++ condition-api/src/main/java/com/mediconnect/condition_api/config/WebConfig.java
@@ -3,9 +3,13 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@EnableWebMvc
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -18,4 +22,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(0, customJackson2HttpMessageConverter);
     }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
 }
